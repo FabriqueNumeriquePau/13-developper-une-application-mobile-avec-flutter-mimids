4595a3b2f27794971ae6fdb1d25e1f1205088ff4
mimids <infods.mimi@gmail.com>
2022-11-16 09:25:03 +0100

    menu bottom ex1

--- event/lib/main.dart
+++ event/lib/main.dart
@@ -1,31 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class Controller extends GetxController {
+  var selected = 0.obs;
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final PageController pager = PageController();
+    var state = Get.put(Controller());
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("TOP Menu"),
+          ),
+          body: PageView(
+            controller: pager,
+            children: const <Widget>[
+              Home(),
+              Page1(),
+              Page2(),
+            ],
+            onPageChanged: (int i) {
+              state.selected.value = i;
+            },
+          ),
+          bottomNavigationBar: Obx(() => BottomNavigationBar(
+                items: const [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: 'Home'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.access_time), label: 'Page1'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.accessibility_new), label: 'Page2'),
+                ],
+                currentIndex: state.selected.value,
+                onTap: (int i) {
+                  state.selected.value = i;
+                  pager.jumpToPage(i);
+                },
+              )),
+        ));
   }
 }
 
@@ -95,13 +120,21 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.network(
+                'https://cdn.pixabay.com/photo/2017/03/01/15/40/internationalwomensday-2108838_960_720.png'),
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+
+            Image.network(
+                'https://cdn.pixabay.com/photo/2017/03/01/15/41/internationalwomensday-2108840_960_720.png'),
+            const Text(
+              'You have pushed the button this many times:',
             ),
+            // Text(showTimePicker(context: context, initialTime: initialTime)
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headline4,
+            // ),
           ],
         ),
       ),
@@ -110,6 +143,105 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// ---------- Home ----------
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final items = List<String>.generate(10000, (i) => "Item $i");
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blueAccent, Colors.white]),
+        ),
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: const Icon(Icons.phone),
+              title: Text(items[index]),
+            );
+          },
+        ));
+    //   child: const Text('Home',
+    //       style: TextStyle(color: Colors.white, fontSize: 32.0)),
+    // );
+  }
+}
+
+// ---------- Page1 ----------
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final myController = TextEditingController();
+    return Container(
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.green, Colors.white]),
+      ),
+      child: Column(
+        children: [
+                    const SizedBox(height: 20.0,),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'name',
+            ),
+            onChanged: (text) {},
+          ),
+                    const SizedBox(height: 20.0,),
+          TextField(
+            controller: myController,
+            decoration: const InputDecoration(
+              hintText: 'hobby',
+            ),
+          ),
+          const SizedBox(height: 20.0,),
+          ElevatedButton(
+            // style: ElevatedButton.styleFrom(
+            //   foregroundColor: Colors.blueAccent,
+            // ),
+            child:  const Text('new'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RaisedButton {}
+
+// ---------- Page2 ----------
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.purpleAccent, Colors.white]),
+      ),
+      child: const Text('Page2',
+          style: TextStyle(color: Colors.white, fontSize: 32.0)),
     );
   }
 }
